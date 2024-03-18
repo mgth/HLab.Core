af49f257db1908c105ebdc8ceb476a3da30f10ec
Mathieu GRENET <sub@mgth.fr>
2024-03-18 09:02:21 +0100

    Add Vector process

---
+++ HLab.Base/LinqExtensions.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace HLab.Base;
+
+public static class LinqExtensions
+{
+    public static (int min, int max) GetMinMax<T>(this IEnumerable<T> @this, Func<T,int> getter)
+    {
+        using var current = @this.GetEnumerator();
+        if(!current.MoveNext()) return (0,0);
+        var value = getter(current.Current);
+        var min = value;
+        var max = value;
+        while (current.MoveNext())
+        {
+            value = getter(current.Current);
+            if (value > max) max = value;
+            else if (value < min) min = value;
+        }
+        return (min, max);
+    }
+}
---
+++ HLab.Base/Vectors/VectorsExtension.cs
@@ -0,0 +1,30 @@
+﻿using System.Numerics;
+using System.Runtime.Intrinsics;
+
+namespace HLab.Base.Vectors;
+
+public static class VectorConstants
+{
+   #if NET8_0_OR_GREATER
+   public static Vector512<int> Range_0_15 = Vector512.Create(0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15);
+   public static Vector512<int> Value2 = Vector512.Create(2);
+   public static Vector512<int> Value4 = Vector512.Create(4);
+   public static Vector512<int> Value8 = Vector512.Create(8);
+   public static Vector512<int> Value16 = Vector512.Create(16);
+   public static Vector512<int> Value32 = Vector512.Create(32);
+   public static Vector512<int> Value64 = Vector512.Create(64);
+   #endif
+}
+
+
+public static class VectorsExtension
+{
+    public static Vector<int> Modulo(this Vector<int> @this, Vector<int> div) => @this - (@this / div) * div;
+    public static Vector64<int> Modulo(this Vector64<int> @this, Vector64<int> div) => @this - (@this / div) * div;
+    public static Vector128<int> Modulo(this Vector128<int> @this, Vector128<int> div) => @this - (@this / div) * div;
+    public static Vector256<int> Modulo(this Vector256<int> @this, Vector256<int> div) => @this - (@this / div) * div;
+
+    #if NET8_0_OR_GREATER
+    public static Vector512<int> Modulo(this Vector512<int> @this, Vector512<int> div) => @this - (@this / div) * div;
+    #endif
+}
