e06bcedb38e14ba6559a7aba72ee4e210889e0af
Mathieu GRENET <sub@mgth.fr>
2024-05-21 12:16:06 +0200

    implement 8bits bosh maps

--- HLab.Base/SortedQueue.cs
+++ HLab.Base/SortedQueue.cs
@@ -1,10 +1,15 @@
 ﻿#nullable enable
 using System;
+using System.Collections.Generic;
 
 namespace HLab.Base;
 
 public class SortedQueue<T>(Func<T, T, int> comparator)
 {
+    public SortedQueue() : this((a, b) => Comparer<T>.Default.Compare(a, b))
+    {
+    }
+
     class Node        
     {
         public Node? Next;
@@ -18,7 +23,7 @@ public class SortedQueue<T>(Func<T, T, int> comparator)
     public void Enqueue(T item)
     {
         ref var node = ref _head;
-        while(node is { } && Comparator(item,node.Value) > 0)
+        while(node is not null && Comparator(item,node.Value) > 0)
         {
             node = ref node.Next;
         }
@@ -36,4 +41,32 @@ public class SortedQueue<T>(Func<T, T, int> comparator)
         item = node.Value;
         return true;
     }
+    public bool TryDequeue(out T? item, Func<T,bool> condition)
+    {
+        var node = _head;
+        if (node == null) {
+            item = default;
+            return false;
+        }
+        if(!condition(node.Value))
+        {
+            item = default;
+            return false;
+        }
+        _head = node.Next;
+        item = node.Value;
+        return true;
+    }
+
+    public bool TryPeek(out T? item)
+    {
+        var node = _head;
+        if (node == null) {
+            item = default;
+            return false;
+        }
+        item = node.Value;
+        return true;
+    }
+
 }
