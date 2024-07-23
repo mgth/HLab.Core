278973ef217d94e160e741f3c42a1fb26ede8678
Mathieu GRENET <sub@mgth.fr>
2024-07-23 17:42:14 +0200

    update avalonia 11.1

---
+++ HLab.Base/HLabDebug.cs
@@ -0,0 +1,15 @@
+﻿using System.Diagnostics;
+
+namespace HLab.Base
+{
+    public static class HLabDebug
+    {
+        [DebuggerHidden]
+        [Conditional("DEBUG")]
+        public static void DebugBreak()
+        {
+            if (System.Diagnostics.Debugger.IsAttached)
+                System.Diagnostics.Debugger.Break();
+        }
+    }
+}
